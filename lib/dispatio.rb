45fee8cc17ce449d112000fd80726857a3c9d7e4
John Mettraux <jmettraux@gmail.com>
2026-09-11 10:46:30 +0900

    Make it work with instances

--- lib/dispatio.rb
+++ lib/dispatio.rb
@@ -2,5 +2,37 @@
 module Dispatio
 
   VERSION = '1.0.0'
+
+  class << self
+
+    def make_table(point, prefix)
+
+      Dispatio::Table.new(
+        prefix,
+        point
+          .methods
+          .select { |m|
+            m.to_s.start_with?(prefix) }
+          .inject({}) { |h, m|
+            h[m.to_s[prefix.length..-1]] = point.method(m)
+            h })
+    end
+  end
+
+  class Table
+
+    def initialize(prefix, table)
+
+      @prefix = prefix
+      @table = table
+    end
+
+    def dispatch(name, *args, **opts, &block)
+
+      ( @table[name.to_s] ||
+        fail(NoMethodError.new("no :#{@prefix}#{name} method"))
+          ).call(*args, **opts, &block)
+    end
+  end
 end
 
--- test/dispatio_test.rb
+++ test/dispatio_test.rb
@@ -7,9 +7,35 @@
 
 group 'Dispatio' do
 
-  test 'scaffold' do
+  class Foo
 
-    assert true
+    def consume(name, data)
+
+      dtable.dispatch(name, data)
+    end
+
+    protected
+
+    def validate_foo(data)
+      [ self.class, :vfoo, data ]
+    end
+
+    def validate_bar(data)
+      [ self.class, :vbar, data ]
+    end
+
+    def dtable; @dtable ||= Dispatio.make_table(self, 'validate_'); end
   end
+
+  test 'against instance' do
+
+    foo = Foo.new
+
+    assert foo.consume(:foo, 'hello'), [ Foo, :vfoo, 'hello' ]
+  end
+
+  #test 'against singleton class' do
+  #  assert foo.consume(:foo, 'hello'), [ Foo, :vfoo, 'hello' ]
+  #end
 end
 
